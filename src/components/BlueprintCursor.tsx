b004aa7b390479b1c66727f833e09d9851f9ce04
Dpatel0805693 <143839560+Dpatel0805693@users.noreply.github.com>
2026-07-21 12:29:40 -0400

    Updated the cursor

--- src/components/BlueprintCursor.tsx
+++ src/components/BlueprintCursor.tsx
@@ -9,104 +9,101 @@ export default function BlueprintCursor() {
 
   const x = useSpring(mouseX, {
     stiffness: 180,
-    damping: 25,
+    damping: 24,
     mass: 0.3,
   });
 
   const y = useSpring(mouseY, {
     stiffness: 180,
-    damping: 25,
+    damping: 24,
     mass: 0.3,
   });
 
   useEffect(() => {
-    const move = (e: MouseEvent) => {
+    const handleMouseMove = (e: MouseEvent) => {
       mouseX.set(e.clientX);
       mouseY.set(e.clientY);
     };
 
-    window.addEventListener("mousemove", move);
+    window.addEventListener("mousemove", handleMouseMove);
 
-    return () => window.removeEventListener("mousemove", move);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
   }, []);
 
   return (
     <motion.div
       className="fixed pointer-events-none z-40"
-      style={{
-        x,
-        y,
-      }}
+      style={{ x, y }}
     >
+      {/* Infinite Horizontal Blueprint Line */}
+      <div
+        className="absolute left-1/2 top-1/2 h-px -translate-x-1/2 -translate-y-1/2 bg-white/15 overflow-hidden"
+        style={{ width: "5000px" }}
+      >
+        <motion.div
+          className="absolute top-1/2 h-[4px] w-[4px] -translate-y-1/2 rounded-full bg-white"
+          animate={{
+            x: [-2500, 2500],
+            opacity: [0, 1, 1, 0],
+          }}
+          transition={{
+            duration: 7,
+            repeat: Infinity,
+            ease: "linear",
+          }}
+        />
+      </div>
+
+      {/* Infinite Vertical Blueprint Line */}
+      <div
+        className="absolute left-1/2 top-1/2 w-px -translate-x-1/2 -translate-y-1/2 bg-white/15 overflow-hidden"
+        style={{ height: "5000px" }}
+      >
+        <motion.div
+          className="absolute left-1/2 h-[4px] w-[4px] -translate-x-1/2 rounded-full bg-white"
+          animate={{
+            y: [-2500, 2500],
+            opacity: [0, 1, 1, 0],
+          }}
+          transition={{
+            duration: 8,
+            repeat: Infinity,
+            ease: "linear",
+          }}
+        />
+      </div>
+
       {/* Outer Circle */}
       <motion.div
-        className="
-          absolute
-          -translate-x-1/2
-          -translate-y-1/2
-          w-20
-          h-20
-          rounded-full
-          border
-          border-white/15
-        "
+        className="absolute left-1/2 top-1/2 h-32 w-32 -translate-x-1/2 -translate-y-1/2 rounded-full border border-white/15"
         animate={{
-          scale: [1, 1.03, 1],
+          scale: [1, 1.08, 1],
+          opacity: [0.15, 0.3, 0.15],
         }}
         transition={{
-          duration: 6,
+          duration: 5,
           repeat: Infinity,
           ease: "easeInOut",
         }}
       />
 
-      {/* Horizontal Line */}
-      <div
-        className="
-          absolute
-          -translate-x-1/2
-          w-72
-          h-px
-          bg-white/15
-        "
-      />
-
-      {/* Vertical Line */}
-      <div
-        className="
-          absolute
-          -translate-y-1/2
-          h-72
-          w-px
-          bg-white/15
-        "
-      />
-
-      {/* Small Inner Circle */}
-      <div
-        className="
-          absolute
-          -translate-x-1/2
-          -translate-y-1/2
-          w-3
-          h-3
-          rounded-full
-          border
-          border-white/30
-        "
-      />
+      {/* Inner Circle */}
+      <div className="absolute left-1/2 top-1/2 h-6 w-6 -translate-x-1/2 -translate-y-1/2 rounded-full border border-white/30" />
 
       {/* Center Dot */}
-      <div
-        className="
-          absolute
-          -translate-x-1/2
-          -translate-y-1/2
-          w-[4px]
-          h-[4px]
-          rounded-full
-          bg-white/70
-        "
+      <motion.div
+        className="absolute left-1/2 top-1/2 h-[7px] w-[7px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-white"
+        animate={{
+          scale: [1, 1.6, 1],
+          opacity: [0.6, 1, 0.6],
+        }}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          ease: "easeInOut",
+        }}
       />
     </motion.div>
   );
